f21bffba48651278a9fb9fe32d622dbebb2fd602
Diinicabdullahi1 <Diinicabdullahi@gmail.com>
2025-10-16 20:06:44 +0100

    added stablity

--- infra/main.tf
+++ infra/main.tf
@@ -1,21 +1,21 @@
-{
+provider "azurerm" {
   features {}
+  subscription_id = "f230c61f-0025-4bd8-a85c-269663fb293b"
 }
+
 resource "azurerm_resource_group" "hub_rg" {
   name     = "smart-invite-hub-rg"
   location = "UK South"
 }
 
 resource "azurerm_container_registry" "acr" {
-  name                = "smartinviteacr${random_integer.unique_suffix.result}"
+  name                = "smartinviteacr59602"
   resource_group_name = azurerm_resource_group.hub_rg.name
   location            = azurerm_resource_group.hub_rg.location
   sku                 = "Basic"
   admin_enabled       = true
-}
 
-# Generate a unique suffix for the container registry name, this allows it to be automation friendly as you don't have to manually make sure its unique
-resource "random_integer" "unique_suffix" {
-  min = 10000
-  max = 99999
+  lifecycle {
+    prevent_destroy = true
+  }
 }
